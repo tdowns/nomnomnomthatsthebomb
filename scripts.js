68ea38e56fbc4910e9f7f4a48054bdc507c06f6a
tdowns <tdowns@users.noreply.github.com>
2023-03-16 22:07:30 -0700

    Update scripts.js
    revert to original js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     let carouselIndex = 0;
     const carouselImages = $(".carousel-image");
-    carouselImages.eq(carouselIndex).addClass("carousel-image-active");
 
     function changeCarouselImage() {
-        carouselImages.eq(carouselIndex).removeClass("carousel-image-active");
+        carouselImages.eq(carouselIndex).fadeOut(1000);
         carouselIndex = (carouselIndex + 1) % carouselImages.length;
-        carouselImages.eq(carouselIndex).addClass("carousel-image-active");
+        carouselImages.eq(carouselIndex).fadeIn(1000);
     }
 
     setInterval(changeCarouselImage, 5000); // Change image every 5 seconds
